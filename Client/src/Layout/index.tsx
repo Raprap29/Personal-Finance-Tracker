b744a0997b2dd69a23b73bbf3fb3883d9e793e96
raprap <raprap>
2025-02-13 18:47:52 +0800

    Update

--- Client/src/Layout/index.tsx
+++ Client/src/Layout/index.tsx
@@ -1,7 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { FC } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 
-export const Layout = () => {
+export const Layout: FC = () => {
+    const location = useLocation();
+    const isAuthPage = ['/', '/register'].includes(location.pathname);
+
+    if (isAuthPage) {
+        return (
+          <div className="min-h-screen bg-gray-50">
+            Not Login
+            <Outlet />
+          </div>
+        );
+    }
     return (
         <>
             <Outlet />
